eaca87242412975954697c5a3a10da52f42c5ad6
romuloabbiati <rhabbiati@hotmail.com>
2023-01-01 21:50:27 +0000

    first commit

---
+++ src/main/java/com/smartgroup/smartbilling/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.smartgroup.smartbilling;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartbillingApplication.class);
+	}
+
+}
