cd93bf2db4d82430a458b38f7ba80bfce46ebad6
romuloabbiati <rhabbiati@hotmail.com>
2023-01-03 04:19:13 +0000

    Added thymeleaf tags to Register.html

--- src/main/java/com/smartgroup/smartbilling/controllers/BillController.java
+++ src/main/java/com/smartgroup/smartbilling/controllers/BillController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.smartgroup.smartbilling.model.Bill;
 import com.smartgroup.smartbilling.repositories.BillRepository;
@@ -21,9 +22,12 @@ public class BillController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String save(Bill bill) {
+	public ModelAndView save(Bill bill) {
 		billRepository.save(bill);
-		return "Register";
+		
+		ModelAndView modelAndView = new ModelAndView("Register");
+		modelAndView.addObject("message", "The bill was saved successfully!");
+		return modelAndView;
 	}
 	
 }
