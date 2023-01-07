ab4ea0dd361a9dcb07d02a1f0f816f02460a5116
romuloabbiati <rhabbiati@hotmail.com>
2023-01-07 05:17:27 +0000

    FindAll Bills fix

--- src/main/java/com/smartgroup/smartbilling/controllers/BillController.java
+++ src/main/java/com/smartgroup/smartbilling/controllers/BillController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,10 +38,13 @@ public class BillController {
 	}
 	
 	@RequestMapping
+	@Transactional(readOnly = true)
 	public ModelAndView search() {
 		List<Bill> allBills = billRepository.findAll();
+		
 		ModelAndView modelAndView = new ModelAndView("Search");
 		modelAndView.addObject("bills", allBills);
+
 		return modelAndView;
 	}
 	
--- src/main/java/com/smartgroup/smartbilling/repositories/BillRepository.java
+++ src/main/java/com/smartgroup/smartbilling/repositories/BillRepository.java
@@ -1,11 +1,9 @@
 package com.smartgroup.smartbilling.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.smartgroup.smartbilling.model.Bill;
 
-@Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
 
 }
