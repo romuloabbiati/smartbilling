a73e6b27b17de11718bc7b368c7fb4df06f3c9ca
romuloabbiati <rhabbiati@hotmail.com>
2023-01-19 05:47:57 +0000

    Thymeleaf tags & tomcat config

--- src/main/java/com/smartgroup/smartbilling/SmartbillingApplication.java
+++ src/main/java/com/smartgroup/smartbilling/SmartbillingApplication.java
@@ -4,12 +4,17 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-public class SmartbillingApplication {
+public class SmartbillingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartbillingApplication.class, args);
@@ -18,6 +23,21 @@ public class SmartbillingApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("en", "GB"));
+	}
+	
+	@Configuration
+	public static class MvcConfig implements WebMvcConfigurer {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "bills");
+		}
+		
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartbillingApplication.class);
 	}
 	
 //	@Bean
